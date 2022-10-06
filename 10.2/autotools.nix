64969ac4f1995a86af70efbc124dc9dcefd4954b
gengshuhui <shuhuigeng@163.com>
2022-10-06 11:21:19 +0000

    add 10 and ammend 9

--- 10.2/autotools.nix
+++ 10.2/autotools.nix
@@ -3,6 +3,7 @@ pkgs: attrs:
   let defaultAttrs = {
 	  builder = "${bash}/bin/bash";
 	  args = [ ./builder.sh ];
+	  setup = ./setup.sh;
 	  baseInputs = [ gnutar gzip gnumake gcc coreutils gawk gnused gnugrep binutils.bintools findutils patchelf ];
 	  buildInputs = [ ];
 	  system = builtins.currentSystem;
